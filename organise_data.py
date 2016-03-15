0377145f51c283734b420195b3feeed07ab7da9e
AnthonyCheetham <anthony.cheetham@unige.ch>
2016-03-15 10:45:15 +0100

    Cleaned up organise_data.py and added a function to un-organise the data in case we want to change something

--- organise_data.py
+++ organise_data.py
@@ -1,79 +1,67 @@
 # -*- coding: utf-8 -*-
 """
-This module contains all of the tools useful for sorting the data
+This module contains all of the tools useful for sorting the data.
+Note that the important detect_filetype module is in monitor.py
+
+The two main modules are a data sorter, and a data un-sorter.
+The unsorter should be useful in case you want to change the file structure or
+to undo any changes.
+
+For both modules, you can set dry_run=True to make it print any proposed
+file moves without actually moving them
 """
-import numpy as np
-import sys,os,glob,time,astropy
+import os,glob
 import astropy.io.fits as pyfits
 from astropy.time import Time
+from monitor import detect_filetype
 
-# Some constants
-nexpo_limit=2 # If nexpo > 2, this indicates that it is a target observation. otherwise, sky
-obstime_limits=[0.1,0.5] # all target/sky observations have exp times in this range
+###############
 
-def detect_filetype(hdr,get_folder_string=False):
-    ''' Works out what kind of file it is based on the header'''
-    type_flag=hdr['HIERARCH ESO DPR TYPE']
-    expt=hdr['EXPTIME'] # exposure time.
+def unorganise_data(folder,prefix='NACO_',suffix='.fits',dry_run=True,
+                    save_dir='/Users/cheetham/data/naco_data/GTO/New/'):
+                        
+    ''' 
+    This undoes the effects of organise_data, an puts all of the files into 
+    a single directory. This is most useful for testing changes and fixing bugs
+    in organise_data
+    '''
 
-    try:
-        nexpo=hdr['HIERARCH ESO SEQ NEXPO']
-    except:
-        nexpo=0
+    # Add a slash in case it is missing    
+    if folder[-1]!='/':
+        folder=folder+'/'
+        
+    files=glob.glob(folder+'*/*/*/*/'+prefix+'*'+suffix)
+    calib_files=glob.glob(folder+'*/*/*/'+prefix+'*'+suffix)
+    files.extend(calib_files)
     
+    print len(files),'Files found'
     
-    # Now format all of these strings      
-    if type_flag=='OBJECT':
-        # We need to work out which of the "OBJECT" frames are skies, flux 
-        #  frames and actual target observations.
-        # For the GTO, we can use the number of exposures. Skies are a single cube for the AGPM.
-        # For non-AGPM observations, we don't need separate skies, so label them all as Targ.
-        if nexpo > nexpo_limit:
-            obstype='Target'
-            folder_string='Raw'
-        elif (expt < obstime_limits[1]) and (expt > obstime_limits[0]):
-            obstype='Sky'
-            folder_string='Sky'
+    # Loop through the files and move them
+    for filename in files:
+        
+        new_name=filename.rsplit('/',1)[1]
+        new_name=save_dir+new_name
+        
+        if dry_run:
+            print 'Moving to:',new_name
         else:
-            obstype='Flux'
-            folder_string='Flux'
-    elif type_flag=='SKY':
-        obstype='Sky'
-        folder_string='Sky'
-    elif type_flag=='FLAT,LAMP' or type_flag=='FLAT,SKY':
-        obstype='Flat'
-        folder_string='Flats'
-    
-    # We don't actually use any of the following types, but I thought we might as well
-    # put them somewhere
-    elif type_flag=='STD':
-        obstype='Std'
-        folder_string='STD'
-    elif type_flag=='DARK':
-        obstype='Dark'
-        folder_string='Dark'
-    else:
-        # Put all of the unknown file types into a single folder to make it easy
-        print 'Unrecognised DPR type:',type_flag
-        obstype='Unknown'
-        folder_string='Uncategorized'
-    
-    if get_folder_string:
-        return obstype,folder_string
-    else:
-        return obstype
-    
+            try:
+                os.rename(filename,new_name)
+            except:
+                print("Failed to move:",filename,'to',new_name)
 
-def organise_data(folder,prefix='NACO_CORO',suffix='.fits',dry_run=True,
-              save_dir='/Users/cheetham/data/data_archive/naco_watch_folder/',
-              flux_exp_cutoff=0.15):
+###############
+
+###############    
+
+def organise_data(folder,prefix='NACO_',suffix='.fits',dry_run=True,
+              save_dir='/Users/cheetham/data/naco_data/GTO/New/'):
     """ 
     Looks at all fits files in a folder and moves them to a consistent 
     directory structure.
     
     dry_run: if this is set to True, no files will be moved, but their 
         suggested locations will be printed
-    flux_exp_cutoff: The smallest
 
     Calib/[date]/[type]/Raw/ : the location of raw calibration data. The current 
         types are: flats.
@@ -110,11 +98,10 @@ def organise_data(folder,prefix='NACO_CORO',suffix='.fits',dry_run=True,
         
         # Get the date in a nice format and convert so that observations made in
         #  the same night have the same date.
-        # Chile is UTC-3 all year round (no daylight savings), so doing
-        #  (time-9hrs) moves midnight Chilean time to midday UTC. Then, the raw date
-        #  (i.e. ignoring the time) will give the date of the *night*
+        # MJD is in UTC, so split by midday by subtracting 12hrs and then 
+        # taking the *raw date* .
         
-        date_time=Time(mjd-9./24.,format='mjd')
+        date_time=Time(mjd-0.5,format='mjd')
         date,dummy=date_time.iso.split(' ')
         
         if science_flag=='SCIENCE':
@@ -143,8 +130,12 @@ def organise_data(folder,prefix='NACO_CORO',suffix='.fits',dry_run=True,
         
     return hdr
     
+###############
+    
 # As a test, run it on the watch folder
-folder='/Users/cheetham/data/data_archive/naco_watch_folder/New/'
-folder='/Users/cheetham/data/naco_data/naco_51Eri/Raw/data_with_raw_calibs/'
+#folder='/Users/cheetham/data/data_archive/naco_watch_folder/New/'
+#folder='/Users/cheetham/data/naco_data/GTO/New/'
+#save_dir='/Users/cheetham/data/naco_data/GTO/'
 
-hdr=organise_data(folder,dry_run=False,save_dir='/Users/cheetham/data/naco_data/naco_51Eri/Raw/',prefix='NACO.')
+#hdr=unorganise_data(save_dir,dry_run=False,save_dir=folder,prefix='NACO.',suffix='.fits')
+#hdr=organise_data(folder,dry_run=False,save_dir=save_dir,prefix='NACO',suffix='.fits')
