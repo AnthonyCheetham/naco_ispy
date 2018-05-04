37cd8bbcf5e07450cb06627f0010c29919bfe1a4
AnthonyCheetham <anthony.cheetham@unige.ch>
2018-05-04 11:51:52 +0200

    Fixed bug in script to test agpm centring methods

--- tests_on_data/agpm_centring_methods.sh
+++ tests_on_data/agpm_centring_methods.sh
@@ -3,13 +3,14 @@
 GRAPHIC_N_CORES="-n 6"
 
 OUTPUT_FILE="../centring_test.txt"
+N_TARG="$(ls -l Targ/NACO*.fits | wc -l)"
 
 # The old centring method:
 cd Targ
 rm cube-info/all_info_*.rdb # delete the o_ info files and create new ones
 rm rc_*.fits
 mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_naco_register_"$GRAPHIC_VERSION".py" --pattern o_ -naco -remove_striping --search_region 50 >> $OUTPUT_FILE
-mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_recenter_cubes_"$GRAPHIC_VERSION".py" --pattern nomed_ --info_pattern all_info_ --naxis3 248 --lmax 600  >> $OUTPUT_FILE
+mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_recenter_cubes_"$GRAPHIC_VERSION".py" --pattern nomed_ --info_pattern all_info_ --naxis3 N_TARG --lmax 600  >> $OUTPUT_FILE
 mpirun python $GRAPHIC_DIR"GRAPHIC_convert_for_pca.py" --pattern rc --output_dir "../ADI_old_agpm/"  >> $OUTPUT_FILE
 cd ..
 
@@ -18,7 +19,7 @@ cd Targ
 rm cube-info/all_info_*.rdb # delete the o_ info files and create new ones
 rm rc_*.fits
 mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_naco_register_"$GRAPHIC_VERSION".py" --pattern nomed_ -naco -agpm_centre --search_region 50 >> $OUTPUT_FILE
-mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_recenter_cubes_"$GRAPHIC_VERSION".py" --pattern nomed_ --info_pattern all_info_ --naxis3 248 --lmax 600  >> $OUTPUT_FILE
+mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_recenter_cubes_"$GRAPHIC_VERSION".py" --pattern nomed_ --info_pattern all_info_ --naxis3 N_TARG --lmax 600  >> $OUTPUT_FILE
 mpirun python $GRAPHIC_DIR"GRAPHIC_convert_for_pca.py" --pattern rc --output_dir "../ADI_old_gauss/"  >> $OUTPUT_FILE
 cd ..
 
@@ -30,14 +31,14 @@ cd ..
 cd Targ
 rm rc_*.fits
 mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_naco_agpm_register_"$GRAPHIC_VERSION".py" --pattern nomed  >> $OUTPUT_FILE
-mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_recenter_cubes_"$GRAPHIC_VERSION".py" --pattern nomed_ --info_pattern all_info_ --naxis3 248 --lmax 600  >> $OUTPUT_FILE
+mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_recenter_cubes_"$GRAPHIC_VERSION".py" --pattern nomed_ --info_pattern all_info_ --naxis3 N_TARG --lmax 600  >> $OUTPUT_FILE
 mpirun python $GRAPHIC_DIR"GRAPHIC_convert_for_pca.py" --pattern rc --output_dir "../ADI_new_gauss/"  >> $OUTPUT_FILE
 cd ..
 
 # The new method, centred on the AGPM
 cd Targ
 rm rc_*.fits
-mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_recenter_cubes_"$GRAPHIC_VERSION".py" --pattern nomed_ --info_pattern all_info_ --naxis3 248 --lmax 600 -nofit  >> $OUTPUT_FILE
+mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_recenter_cubes_"$GRAPHIC_VERSION".py" --pattern nomed_ --info_pattern all_info_ --naxis3 N_TARG --lmax 600 -nofit  >> $OUTPUT_FILE
 mpirun python $GRAPHIC_DIR"GRAPHIC_convert_for_pca.py" --pattern rc --output_dir "../ADI_new_agpm/"  >> $OUTPUT_FILE
 cd ..
 
