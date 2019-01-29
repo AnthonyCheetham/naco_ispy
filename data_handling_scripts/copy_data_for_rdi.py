131ac44946f94a030be78c3dbbb2705b76de19af
AnthonyCheetham <anthony.cheetham@unige.ch>
2019-01-29 14:55:13 +0100

    Fixed directories in copy_data_for_rdi

--- data_handling_scripts/copy_data_for_rdi.py
+++ data_handling_scripts/copy_data_for_rdi.py
@@ -20,11 +20,12 @@ import subprocess
 
 
 data_dir = '/data/NACO/Science/'
-data_dir = '/Users/cheetham/data/naco_data/GTO/Science/'
+save_dir = '/home/spectro/cheetham/tmp/rdi_cubes/'
+
+#data_dir = '/Users/cheetham/data/naco_data/GTO/Science/'
+#save_dir = '/Users/cheetham/tmp/rdi_cubes/'
 
 dirs = glob.glob(data_dir+'*/*/ADI/')
-
-save_dir = '/Users/cheetham/tmp/rdi_cubes/'
 
 output_radius = 25 # so a 50x50 image
 
