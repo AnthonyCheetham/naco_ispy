0b8f53a4474c4c6fa0805e0b0ed7b8dd11c72e5f
AnthonyCheetham <anthony.cheetham@unige.ch>
2019-06-11 12:15:18 +0200

    Added script to test 1D AGPM centring method

--- tests_on_data/agpm_centring_methods.sh
+++ tests_on_data/agpm_centring_methods.sh
@@ -36,6 +36,19 @@ mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_recenter_cubes_"$GRAPHIC_VER
 mpirun python $GRAPHIC_DIR"GRAPHIC_convert_for_pca.py" --pattern rc --output_dir "../ADI_new_gauss/"  >> $OUTPUT_FILE
 cd ..
 
+# The new double-gaussian method with 1D fitting:
+cd Sky
+mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_naco_agpm_offset_"$GRAPHIC_VERSION".py" --pattern sky-num/med1 -fit_1d  >> $OUTPUT_FILE
+cd ..
+cd Targ
+rm cube-info/all_info_*.rdb # delete the o_ info files and create new ones
+rm rc_*.fits
+mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_naco_agpm_register_"$GRAPHIC_VERSION".py" --pattern nomed -fit_1d  >> $OUTPUT_FILE
+mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_recenter_cubes_"$GRAPHIC_VERSION".py" --pattern nomed_ --info_pattern all_info_ --naxis3 $N_TARG --lmax 600  >> $OUTPUT_FILE
+mpirun python $GRAPHIC_DIR"GRAPHIC_convert_for_pca.py" --pattern rc --output_dir "../ADI_new_gauss/"  >> $OUTPUT_FILE
+cd ..
+
+
 # The new method, centred on the AGPM
 cd Targ
 rm rc_*.fits
@@ -43,4 +56,3 @@ mpirun $GRAPHIC_N_CORES python $GRAPHIC_DIR"GRAPHIC_recenter_cubes_"$GRAPHIC_VER
 mpirun python $GRAPHIC_DIR"GRAPHIC_convert_for_pca.py" --pattern rc --output_dir "../ADI_new_agpm/"  >> $OUTPUT_FILE
 cd ..
 
-#python $GRAPHIC_DIR"GRAPHIC_pca.py" --output_dir "./GRAPHIC_PCA/" --threads 6 --arc_length 1e4 --n_annuli 35 -median_combine --n_fwhm 0.75 --n_modes 0.3 --min_reference_frames 0.3 --fwhm 4. --r_min 3. --r_max 299.
