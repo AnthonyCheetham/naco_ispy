8fcd7c9077807f7e2bbd48b5c5e0901df5db25f6
AnthonyCheetham <anthony.cheetham@unige.ch>
2019-06-14 11:36:37 +0200

    Added MPIA version of update_obs_table

---
+++ data_handling_scripts_mpia/update_obs_table.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  9 14:18:56 2016
+
+This will update the observations table when run on the NACO DRS
+
+@author: cheetham
+"""
+
+import naco_ispy
+
+data_folder = '/data/beegfs/astro-storage/groups/henning/cheetham/naco-ispy-shared/'
+db_filename = '/data/beegfs/astro-storage/groups/henning/cheetham/naco-ispy-shared/obs_table.dat'
+
+# Make the object 
+obs_db = naco_ispy.databases.obs_table(filename=db_filename, data_folder=data_folder)
+
+# Run the search function to find the relevant files and update the table
+# Update previous entries if possible
+obs_db.search(dry_run=False, fast_update = True)
+
+# Save the updated table
+obs_db.save()
